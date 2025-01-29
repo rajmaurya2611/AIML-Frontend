5b711b5dd968cd538fb5fe7d4f0b59a5b4cbadae
Maurya <Raj.Maurya@motherson.com>
2025-01-29 18:30:48 +0530

    First commit

---
+++ src/components/homepage/homepage.tsx
@@ -0,0 +1,13 @@
+import { GlobeDemo } from "./ui/firstDiv";
+
+function HomePage() {
+    return (
+      <><div className="p-0 m-0">
+        
+        <GlobeDemo/>
+        </div>
+      </>
+    )
+  }
+  
+  export default HomePage;
